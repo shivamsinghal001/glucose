ac7b1fc81f0e1d70c2c2ed1ca6192e02c4408657
shivamsinghal001 <60418185+shivamsinghal001@users.noreply.github.com>
2022-12-30 00:59:41 -0800

    fix reward fun

--- bgp/simglucose/envs/simglucose_gym_env.py
+++ bgp/simglucose/envs/simglucose_gym_env.py
@@ -110,7 +110,6 @@ class SimglucoseEnv(gym.Env):
         self.meal = config["meal"]
         self.norm = config["norm"]
         self.gt = config["gt"]
-        self.reward_fun = reward_name_to_function(config["reward_fun"])
         self.reward_bias = config["reward_bias"]
         self.observed_reward_fn = reward_name_to_function(config["observed_reward_fun"])
         self.true_reward_fn = reward_name_to_function(config["true_reward_fun"])
